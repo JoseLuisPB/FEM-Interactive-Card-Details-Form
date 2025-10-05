4faeccdaf8c02cbf8e9015f597636c9d2e162207
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2025-10-05 11:35:10 +0200

    Refactor main.js

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
-//TODO => Refactorizar index
-//TODO => Refactorizar styles
-//Todo => Refactorizar main
-
-
-/* Card holder name */
+/* Input selectors*/
 const cardHolderNameInput = document.querySelector('#cardholderName');
+const cardNumberInput = document.querySelector('#cardNumber');
+const monthInput = document.querySelector('#month');
+const yearInput = document.querySelector('#year');
+const cvcInput = document.querySelector('#cvc');
+
+/* Card images selectors */
 const cardImgName = document.querySelector('#cardImgName');
+const cardImgNumber = document.querySelector('#cardImgNumber');
+const cardimgdate = document.querySelector('#cardimgdate');
+const cardimgcvc = document.querySelector('#cardimgcvc');
+
+/* Error selectors */
 const cardholderNameError = document.querySelector('#cardholderNameError');
+const cardNumberError = document.querySelector('#cardNumberError');
+const monthError = document.querySelector('#monthError');
+const yearError = document.querySelector('#yearError');
+const cvcError = document.querySelector('#cvcError');
+
+/* Other variables */
+const cardForm = document.querySelector('#card-form');
+const completedState = document.querySelector('#completed-state');
+let month = '';
+let year = '';
+
+/* Set placeholder values in cards */
 cardImgName.innerHTML = "JANE APPLESEED";
+cardImgNumber.innerHTML = "0000 0000 0000 0000";
+cardimgdate.innerHTML = "00/00";
+cardimgcvc.innerHTML = "000";
 
 cardHolderNameInput.addEventListener('keyup', () => {
   cardImgName.innerHTML = cardHolderNameInput.value;
 });
 
-function validateCardHolderName(){
-  const cardHolderInputHasNumber = /\d/.test(cardHolderNameInput.value);
-  let error = false;
-
-  if(cardHolderNameInput.value.length === 0 || cardHolderInputHasNumber){
-    error = true;
-  }
-
-  handleErrorState(error, cardholderNameError, cardHolderNameInput);
-  return error;
-}
-
-/* Card number*/
-const cardNumberInput = document.querySelector('#cardNumber');
-const cardImgNumber = document.querySelector('#cardImgNumber');
-const cardNumberError = document.querySelector('#cardNumberError');
-cardImgNumber.innerHTML = "0000 0000 0000 0000";
-
 cardNumberInput.addEventListener('keyup', () => {
   cardImgNumber.innerHTML = formatCardNumber(cardNumberInput.value);
+});
+
+monthInput.addEventListener('keyup', () => {
+  month = monthInput.value;
+  updateMonthAndYear();
+});
+
+yearInput.addEventListener('keyup', () => {
+  year = yearInput.value;
+  updateMonthAndYear();
+});
+
+cvc.addEventListener('keyup', () => {
+  cardimgcvc.innerHTML = cvc.value;
 });
 
 function formatCardNumber(cardNumber){
@@ -47,6 +64,36 @@ function formatCardNumber(cardNumber){
   ].join(' ');
 }
 
+function updateMonthAndYear(){
+  cardimgdate.innerHTML = `${month}/${year}`
+}
+
+function submitForm(){
+  const errorChecker = [];
+  errorChecker.push(validateCardHolderName());
+  errorChecker.push(validateCardNumber());
+  errorChecker.push(validateMonth());
+  errorChecker.push(validateYear());
+  errorChecker.push(validateCvc());
+
+  const isFormValid = errorChecker.every(error => error === false);
+  if(isFormValid){
+    completeForm();
+  }
+}
+
+function validateCardHolderName(){
+  const cardHolderInputHasNumber = /\d/.test(cardHolderNameInput.value);
+  let error = false;
+
+  if(cardHolderNameInput.value.length === 0 || cardHolderInputHasNumber){
+    error = true;
+  }
+
+  handleErrorState(error, cardholderNameError, cardHolderNameInput);
+  return error;
+}
+
 function validateCardNumber(){
   let error = false;
 
@@ -55,33 +102,6 @@ function validateCardNumber(){
   }
   handleErrorState(error, cardNumberError, cardNumberInput);
   return error;
-}
-
-
-/* Year and date */
-const cardimgdate = document.querySelector('#cardimgdate');
-const monthInput = document.querySelector('#month');
-const yearInput = document.querySelector('#year');
-const monthError = document.querySelector('#monthError');
-const yearError = document.querySelector('#yearError');
-
-cardimgdate.innerHTML = "00/00";
-
-let month = '';
-let year = '';
-
-monthInput.addEventListener('keyup', () => {
-  month = monthInput.value;
-  updateMonthAndYear();
-});
-
-yearInput.addEventListener('keyup', () => {
-  year = yearInput.value;
-  updateMonthAndYear();
-});
-
-function updateMonthAndYear(){
-  cardimgdate.innerHTML = `${month}/${year}`
 }
 
 function validateMonth(){
@@ -107,18 +127,6 @@ function validateYear(){
   return error;
 }
 
-
-
-/* cvc */
-const cvcInput = document.querySelector('#cvc');
-const cardimgcvc = document.querySelector('#cardimgcvc');
-const cvcError = document.querySelector('#cvcError');
-cardimgcvc.innerHTML = "000"
-
-cvc.addEventListener('keyup', () => {
-  cardimgcvc.innerHTML = cvc.value;
-});
-
 function validateCvc(){
   let error = false;
   if(cvcInput.value.length === 0 || inputHasALetter(cvcInput)){
@@ -129,18 +137,10 @@ function validateCvc(){
   return error;
 }
 
-function submitForm(){
-  const errorChecker = [];
-  errorChecker.push(validateCardHolderName());
-  errorChecker.push(validateCardNumber());
-  errorChecker.push(validateMonth());
-  errorChecker.push(validateYear());
-  errorChecker.push(validateCvc());
-
-  const isFormValid = errorChecker.every(error => error === false);
-  if(isFormValid){
-    completeForm();
-  }
+function completeForm(isFormValid){
+  cardForm.classList.add('hide-form');
+  completedState.classList.remove('hide-completed-state');
+  completedState.classList.add('display-completed-state');
 }
 
 function handleErrorState(error, htmlErrorElement, htmlInputElement){
@@ -161,13 +161,4 @@ function inputHasALetter(inputElement){
 
 function isValidMonthDate(month){
   return +month >= 1 && +month <= 12;
-}
-
-const cardForm = document.querySelector('#card-form');
-const completedState = document.querySelector('#completed-state');
-
-function completeForm(isFormValid){
-  cardForm.classList.add('hide-form');
-  completedState.classList.remove('hide-completed-state');
-  completedState.classList.add('display-completed-state');
 }
